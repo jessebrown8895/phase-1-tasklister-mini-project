af91ff8617ddadd02930986807248d953f520e38
Jesse Brown <Brownjesse8895@yahoo.com>
2021-06-28 22:01:34 -0400

    TaskLister Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form")
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const description = document.getElementById("new-task-description").value 
+    const eTask = document.getElementById("tasks")
+    const list = document.createElement("li")
+    list.innerText = description 
+    eTask.append(list)
+  } )
 });
+
+
+
+
